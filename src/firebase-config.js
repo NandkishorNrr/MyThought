082a9f27263e877b14017fc54e4e98a81f093a8d
Nandkishor Rathore <nrr.tvans@gmail.com>
2023-12-03 14:10:35 +0530

    Project 5: MyThought, completed

---
+++ src/firebase-config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqWM9-xnFaT6VLuJCp75FwlNvQ6ahYcq0",
+  authDomain: "mythought-cb312.firebaseapp.com",
+  projectId: "mythought-cb312",
+  storageBucket: "mythought-cb312.appspot.com",
+  messagingSenderId: "293621219167",
+  appId: "1:293621219167:web:78fe75e7a1c2aa443500ea"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app)
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
---
+++ src/pages/Home.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
+import { auth, db } from "../firebase-config";
+
+function Home({ isAuth }) {
+  const [postLists, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
+
+  const deletePost = async (id) => {
+    const postDoc = doc(db, "posts", id);
+    await deleteDoc(postDoc);
+  };
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPosts();
+  }, [deletePost]);
+
+  // ... (previous code)
+
+  return (
+    <div className="homePage">
+      {postLists.map((post) => {
+        // Add a check to ensure 'post' is defined
+        if (!post || !post.author || !post.author.id) {
+          return null; // Skip rendering if 'post' or 'post.author.id' is undefined
+        }
+
+        return (
+          <div className="post" key={post.id}>
+            <h4>@{post.author.name}</h4>
+            <hr />
+            <div className="postHeader">
+              <div className="title">
+                <h1>{post.title}</h1>
+              </div>
+              <div className="deletePost">
+                {isAuth && post.author.id === auth.currentUser.uid && (
+                  <button
+                    onClick={() => {
+                      deletePost(post.id);
+                    }}
+                  >
+                    &#128465;
+                  </button>
+                )}
+              </div>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+            <div className="reactions">
+              <i class="fa-regular fa-heart">like</i>
+              <i class="fa-light fa-share">share</i>
+              <i class="fa-regular fa-comment">comment</i>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+
+  // ... (remaining code)
+}
+
+export default Home;
